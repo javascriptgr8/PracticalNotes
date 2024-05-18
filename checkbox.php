fc5caa72a24764db079a4f7c88bc364a203e653f
javascriptgr8 <88194115+javascriptgr8@users.noreply.github.com>
2024-05-18 10:13:15 +0530

    Update checkbox.php

--- checkbox.php
+++ checkbox.php
@@ -8,3 +8,15 @@ $("input:checkbox[name=type]:checked").each(function(){
 $("input:checkbox:not(:checked)").each(function(){
   yourArray.push($(this).val());
 });
+
+
+
+
+$(document).on("click",".content",function(){
+    var cid=$(this).attr('data-content');
+    if ($(this).is(':checked')) {
+        $("."+cid).prop('checked', true);
+    } else if($(this).is(':not(:checked)')) {
+        $("."+cid).prop('checked', false);
+    }
+});
